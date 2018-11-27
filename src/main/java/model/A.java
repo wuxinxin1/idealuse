69b69e3c9cbd49ab4f77c7d29adbb5eb9fc3ceda
wuxinxin1 <2468732919@qq.com>
2018-11-27 18:36:03 +0800

    aa

--- src/main/java/model/A.java
+++ src/main/java/model/A.java
@@ -1,8 +1,10 @@
 package model;
 
 /**
- *
  * Created by wuxinxin on 2018/11/27.
  */
 public class A {
+    public  void  m1(){
+        System.out.println("hello mm");
+    }
 }
---
+++ src/main/java/model/B.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.math.BigInteger;
+
+/**
+ * live template的使用
+ * 使用方法：先ctr+shift+a,搜索，然后编辑
+ * Created by wuxinxin on 2018/11/27.
+ */
+public class B {
+    //模板生成
+    private int name;
+    //模板生成
+    private String name1;
+    //模板生成
+    private BigInteger balance;
+
+
+    //定义一个main函数模板
+    public static void main(String[] args){
+
+    }
+    //定义一个常量
+    public static final String NAME="WXX";
+}
