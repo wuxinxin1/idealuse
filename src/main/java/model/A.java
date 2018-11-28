bb4a4c3d9c0bbeea1ed3f843c382a8b922edff89
wuxinxin1 <2468732919@qq.com>
2018-11-28 10:20:34 +0800

    aa

--- src/main/java/model/A.java
+++ src/main/java/model/A.java
@@ -1,10 +1,29 @@
 package model;
 
 /**
+ * postfix的使用
+ *
+ * 使用方法：ctr+shift+a  搜索postfix 修改或者使用默认的
  * Created by wuxinxin on 2018/11/27.
  */
 public class A {
-    public  void  m1(){
-        System.out.println("hello mm");
+
+    /**
+     * 测试if while
+     * 通过一个bool类型的值执行" ."操作
+     * @throws Exception
+     */
+    public void  test1() throws Exception{
+       boolean b=true;
+
+       //b.if
+        if (b) {
+
+        }
+        //b.while
+        while (b) {
+
+        }
+
     }
 }
